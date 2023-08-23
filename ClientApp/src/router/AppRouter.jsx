3272e8edfdacbdf6d9011e9e0f240704688c2877
DnTo <danto@hotmail.com>
2023-08-22 21:10:38 -0500

    init

---
+++ ClientApp/src/router/AppRouter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+// components
+import Home from "../pages/Home";
+
+
+export function AppRouter() {
+  return (
+    <>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+       
+      </Routes>
+    </>
+  );
+}
