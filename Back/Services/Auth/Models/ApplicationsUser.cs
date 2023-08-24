10b38a68b9730c5cdbdf93469223dfeecffe8518
Daniel Torres <danto@hotmail.com>
2023-08-24 17:35:21 -0500

    Auth con google (#2)

---
+++ Back/Services/Auth/Models/ApplicationsUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace s10.Back.Services.Auth.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Key]
+        public int IdCliente { get; private set; }
+
+        public string? Foto { get; set; }
+        public string? Nombres { get; set; }
+        public string? Apellidos { get; set; }
+
+        public string? Direccion { get; set; } = String.Empty;
+        public string? Provincia { get; set; }
+        public string? Pais { get; set; }
+    }
+}
