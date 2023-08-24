4b84693d920d95cdaa735360619b93bd2090ff1e
Ana Juárez <lisa13suarez@gmail.com>
2023-08-23 21:22:20 -0600

    ➕ Adding route

--- ClientApp/src/App/index.jsx
+++ ClientApp/src/App/index.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import { useSelector } from "react-redux";
 
+
 import { PublicRoutes, PrivateRoutes, AuthRouter, AppRouter } from "../router";
+import Report from "../pages/Report/report";
 
 export function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-              <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/report" element={<Report />} />
         </Routes>
     </BrowserRouter>
   );
