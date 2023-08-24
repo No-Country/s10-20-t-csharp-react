44c97ccf3127835c121ec1300dd73efc88e37dad
Octavio Salas <salasoctavio129@gmail.com>
2023-08-24 19:36:54 -0300

    formRegisterAndLogin

--- ClientApp/src/App/index.jsx
+++ ClientApp/src/App/index.jsx
@@ -3,6 +3,8 @@ import Home from "../pages/Home";
 import { useSelector } from "react-redux";
 
 import { PublicRoutes, PrivateRoutes, AuthRouter, AppRouter } from "../router";
+import Register from "../pages/Register";
+import SignIn from "../pages/SignIn";
 
 export function App() {
 
@@ -10,6 +12,9 @@ export function App() {
     <BrowserRouter>
       <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<SignIn />} />
+
         </Routes>
     </BrowserRouter>
   );
