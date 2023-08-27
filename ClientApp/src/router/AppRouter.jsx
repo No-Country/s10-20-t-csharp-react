33330aaf768a1ea77c889dcb596f83ee00fc4057
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-08-27 11:23:55 -0300

    Add a new route when page not found

--- ClientApp/src/router/AppRouter.jsx
+++ ClientApp/src/router/AppRouter.jsx
@@ -1,17 +1,31 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 // components
 import Home from "../pages/Home";
+import Landing from "../pages/landing";
+import { NotFound } from "../pages/404NotFound";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 export function AppRouter() {
-  return (
-    <>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-       
-      </Routes>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
