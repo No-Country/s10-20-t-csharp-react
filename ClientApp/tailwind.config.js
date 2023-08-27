2d4b7411169dd3fae111bce009585c9e89a57dab
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-08-27 11:23:20 -0300

    Feat: add some custom styles

--- ClientApp/tailwind.config.js
+++ ClientApp/tailwind.config.js
@@ -6,19 +6,26 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
+      fontSize: {
+        '2xl': "3rem"
+      },
       colors: {
         white: "#ffffff",
+        "bg-color": "#fff",
         "gray-50": "#ebebeb",
-        "bg-color": "#f5f5f5",
         "gray-100": "#999ca0",
         "gray-150": "#656565",
-        "primary-100": "#1558d6",
         "primary-50": "#e8f0fe",
+        "primary-100": "#184656",
         "primary-150": "#5c9ce8",
         "secondary-50": "#a4d0c0",
         "secondary-100": "#488c73",
+        success: "",
         error: "#ff4e64",
       },
+      borderRadius: {
+        '2xl': "20px"
+      }
     },
   },
   plugins: [],
