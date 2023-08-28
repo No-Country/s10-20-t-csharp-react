efc97b150dadd7c8c5f12c224641f2f3162edabd
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-08-28 09:42:53 -0300

    Add an image with a 404 code

--- ClientApp/src/pages/404NotFound.jsx
+++ ClientApp/src/pages/404NotFound.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
 
+import notFoundImg from "../assets/not_found.jpg";
+
 export function NotFound() {
   return (
     <div className="flex flex-col h-screen justify-center items-center gap-4">
-      <h1 className="text-error text-2xl font-bold">404</h1>
+      <img
+        width="300"
+        height="300"
+        src={notFoundImg}
+        alt="TV showing an 404 error message"
+      />
       <h3 className="text-lg">La página que busca no existe</h3>
       <Link className="text-primary-100" to="/">
         Volver
