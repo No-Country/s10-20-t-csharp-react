fc4e94c1fe4f05c98a667971c027e01582b76970
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-08-28 19:02:12 -0300

    Update the landing view

--- ClientApp/src/pages/landing.jsx
+++ ClientApp/src/pages/landing.jsx
@@ -1,31 +1,81 @@
 import React from "react";
-
-import puzzle from "../../public/assets/puzzle.png";
-import logo from "../../public/assets/logo.svg";
 import { Link } from "react-router-dom";
+
+import puzzle from "../assets/puzzle.png";
+import flechaAbajo from "../assets/icons/flecha.svg";
+import logo from "../assets/logo.svg";
+
+const featuresItems = [
+  "Ayudá a visibilizar las problematicas frecuentes en los bienes publicos de tu ciudad",
+  "Enterate de los reportes de otros ciudadanos",
+  "Controbuí a mantener los espacios comunes en optimas condiciones",
+  "Sé responsable...",
+];
 
 export default function Landing() {
   return (
-    <main className="h-screen p-2 flex justify-center items-center gap-4">
-      <div>
-        <img src={puzzle} alt="Personas sobre un rompecabezas" />
-      </div>
+    <main className="px-20 py-2">
       <div
-        id="content"
-        className="flex flex-col justify-center items-center gap-4"
+        id="hero-section"
+        className="h-screen flex flex-col items-center gap-4 justify-center"
       >
-        <img className="w-96" src={logo} alt="Logo de la página" />
-        <p className="w-[30ch] text-center text-lg">
-          Tu aporte como ciudadano ayuda a visibilizar problemas en los bienes
-          de uso públicos
-        </p>
-        <Link
-          to="/register"
-          className="font-bold text-primary-100 border border-primary-100 p-2 rounded-2xl hover:bg-primary-100 hover:text-white transition-colors"
-        >
-          ¡Hazte oír!
-        </Link>
+        <div className="flex justify-center items-center gap-4">
+          <div id="hero-section__img">
+            <img
+              width="500"
+              height="500"
+              src={puzzle}
+              alt="Personas sobre un rompecabezas"
+            />
+          </div>
+          <div
+            id="hero-section__content"
+            className="flex flex-col justify-center items-center gap-4"
+          >
+            <img className="w-96" src={logo} alt="Logo de la página" />
+            <p className="w-[30ch] text-center text-title-lg">
+              Tu aporte como ciudadano ayuda a visibilizar problemas en los
+              bienes de uso públicos
+            </p>
+            <div className="flex gap-2">
+              <Link
+                to="/register"
+                className="font-bold text-white border bg-terciary-100 p-2 rounded-2xl hover:bg-terciary-50 transition-colors"
+              >
+                Registrarme
+              </Link>
+              <Link
+                to="/login"
+                className="border-terciary-100 font-bold text-terciary-100 border p-2 rounded-2xl hover:border-2 hover:border-terciary-50 transition-colors"
+              >
+                Iniciar sesión
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img
+            className="mt-20"
+            src={flechaAbajo}
+            alt="Flecha apuntando hacia abajo"
+            width="50"
+            height="50"
+          />
+        </div>
       </div>
+
+      <section className="flex flex-col items-center gap-8 py-10">
+        <ul className="flex flex-col gap-4">
+          {featuresItems.map((item, id) => (
+            <li key={id} className="text-title-lg">
+              {item}
+            </li>
+          ))}
+        </ul>
+        <Link className="border border-terciary-100 text-terciary-100 font-bold p-4 rounded-2xl">
+          Ir a reportar
+        </Link>
+      </section>
     </main>
   );
 }
