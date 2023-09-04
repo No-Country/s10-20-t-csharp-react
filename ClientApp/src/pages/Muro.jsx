a2fedf9a21d4fea76e8ab9be57e265fe3adf0a63
Octavio Salas <salasoctavio129@gmail.com>
2023-09-04 15:12:34 -0300

    muroCasiFinalizado

--- ClientApp/src/pages/Muro.jsx
+++ ClientApp/src/pages/Muro.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import { useEffect } from 'react'
 import axios from "axios"
 import MuroPublicationStructure from './MuroPublicationStructure'
+import FiltrosMuro from './FiltrosMuro'
 
 const Muro = () => {
 
@@ -23,46 +24,13 @@ const Muro = () => {
   return (
     <div> 
         <Navbar/>
-        <div className='flex justify-center items-center mt-12 '>
+        <div className='flex justify-center items-center mt-6 '>
              <div className='flex'>
-                 <div className='mr-48'>
-                  <input type="text" className='border-none hover:border-none rounded-lg bg-gray-50 text-black' placeholder='Buscar' />
-                       <div className='mt-6'>
-                            <div className='flex mt-4'>
-                               <input type="checkbox" className="checkbox checkbox-sm" /> 
-                            <p>Aceras</p>
-                            </div>
 
-                            <div className='flex mt-4'>
-                               <input type="checkbox" className="checkbox checkbox-sm" /> 
-                               <p>Limpieza</p>
-                            </div>
-
-                            <div className='flex mt-4'>
-                               <input type="checkbox" className="checkbox checkbox-sm" /> 
-                            <p>Luminarias</p>
-                            </div>
-
-                            <div className='flex mt-4'>
-                                  <input type="checkbox" className="checkbox checkbox-sm" /> 
-                               <p>Parques y Plazas</p>
-                            </div> 
-
-                            <div className='flex mt-4'>
-                                  <input type="checkbox" className="checkbox checkbox-sm" /> 
-                               <p>Transito</p>
-                            </div> 
-
-                            <div className='flex mt-4'>
-                                  <input type="checkbox" className="checkbox checkbox-sm" /> 
-                               <p>Todo</p>
-                            </div> 
-
-                            <div className='mt-4'>
-                              <p className='cursor-pointer text-sm'>Eliminar todos los filtros</p>
-                            </div>
-                       </div>
-                 </div>
+             <div className='flex items-center justify-center h-screen mr-6'> 
+                  <FiltrosMuro/>
+                </div>
+                
 
                  <div >
                   <div className='flex justify-center items-center'>
