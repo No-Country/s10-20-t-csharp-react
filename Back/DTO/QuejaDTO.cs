07ee55b763b6d8e02511acdaedefaf0a36f3aaa7
Daniel Torres <danto@hotmail.com>
2023-09-06 02:01:38 -0500

    Me endoint (#14)

--- Back/DTO/QuejaDTO.cs
+++ Back/DTO/QuejaDTO.cs
@@ -6,9 +6,16 @@
         public string Text { get; set; } = string.Empty;
         public IFormFile? media { get; set; } = null!;
         public int? District_ID { get; set; }
-        public int? Category_ID { get; set; } 
+        public int? Category_ID { get; set; }
         //public int? User_ID { get; set; }
         public double? Longitude { get; set; }
         public double? Latitude { get; set; }
     }
+
 }
+
+
+
+
+
+
--- Migrations/20230906065303_favorites-count.Designer.cs
+++ Migrations/20230906065303_favorites-count.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using s10.Back.Data;
@@ -12,9 +13,10 @@ using s10.Back.Data;
 namespace s10.Migrations
 {
     [DbContext(typeof(RedCoContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230906065303_favorites-count")]
+    partial class favoritescount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
