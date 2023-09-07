28acc3ee1d3d99ffd916da4cd94dfe0e1aac49d7
DnTo <danto@hotmail.com>
2023-09-07 18:09:08 -0500

    add login with email

--- Back/DTO/AppUserDTO.cs
+++ Back/DTO/AppUserDTO.cs
@@ -2,7 +2,7 @@
 {
     public class AppUserDTO
     {
-        public int User_ID { get; set; }
+        public string User_ID { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
     }
