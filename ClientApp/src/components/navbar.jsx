7c9b1141896b46b28d65d15d2d55eee362b8c299
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-09-07 13:31:50 -0300

    Feat: add a modal for the notifications

--- ClientApp/src/components/navbar.jsx
+++ ClientApp/src/components/navbar.jsx
@@ -1,13 +1,44 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 import NotificationIcon from "../assets/icons/notificaciones.svg";
 import PerfilIcon from "../assets/icons/perfil.svg";
+import PerfilImg from "../assets/icons/perfil.png";
+
+const ProfileModal = () => {
+  return (
+    <div className="bg-white p-4 shadow-md absolute top-20 right-0">
+      <Link to="/profile">Mi Perfil</Link>
+    </div>
+  );
+};
+
+const NotificationModal = () => {
+  return (
+    <div className="bg-white p-4 shadow-md absolute top-20 right-0 flex gap-4 border-b-2">
+      <div>
+        <img
+          width={30}
+          height={30}
+          src={PerfilImg}
+          alt="Imagen de perfil del usuario"
+        />
+      </div>
+      <p>
+        <strong>Lorena Casa</strong> ha comentado tu reporte
+      </p>
+    </div>
+  );
+};
 
 export default function Navbar() {
+  const [profileOptions, setProfileOptions] = useState(false);
+  const [notificationesOptions, setNotificationOptions] = useState(false);
+
   return (
     <nav className="flex justify-between items-center w-full p-8">
       <Link to="/">
-        <h3 className="text-primary-100">Red Co.</h3>
+        <h3 className="text-primary-100 text-2xl font-bold">Red Co.</h3>
       </Link>
       <ul className="flex gap-4">
         <Link to="#">
@@ -17,22 +48,32 @@ export default function Navbar() {
           <li>Nuevo Reporte</li>
         </Link>
         <div className="flex gap-4">
-          <Link>
+          <button>
             <img
               width="30"
               height="30"
               src={NotificationIcon}
               alt="icono de las notififaciones"
+              onClick={() => setNotificationOptions(!notificationesOptions)}
             />
-          </Link>
-          <Link>
+          </button>
+          {notificationesOptions ? (
+            <>
+              <NotificationModal />
+            </>
+          ) : null}
+          <button
+            className="relative"
+            onClick={() => setProfileOptions(!profileOptions)}
+          >
             <img
               width="30"
               height="30"
               src={PerfilIcon}
               alt="icono del usuario"
             />
-          </Link>
+          </button>
+          {profileOptions ? <ProfileModal /> : null}
         </div>
       </ul>
     </nav>
