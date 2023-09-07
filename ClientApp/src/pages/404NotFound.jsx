6b26cf03ee6eb8bb4859905a718fada0d045ebed
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-09-07 13:07:53 -0300

    Fix: add the main layout

--- ClientApp/src/pages/404NotFound.jsx
+++ ClientApp/src/pages/404NotFound.jsx
@@ -1,20 +1,23 @@
 import { Link } from "react-router-dom";
 
 import notFoundImg from "../assets/not_found.jpg";
+import { MainLayout } from "../layout/MainLayout";
 
 export function NotFound() {
   return (
-    <div className="flex flex-col h-screen justify-center items-center gap-4">
-      <img
-        width="300"
-        height="300"
-        src={notFoundImg}
-        alt="TV showing an 404 error message"
-      />
-      <h3 className="text-lg">La página que busca no existe</h3>
-      <Link className="text-primary-100" to="/">
-        Volver
-      </Link>
-    </div>
+    <MainLayout>
+      <div className="flex flex-col h-screen justify-center items-center gap-4">
+        <img
+          width="300"
+          height="300"
+          src={notFoundImg}
+          alt="TV showing an 404 error message"
+        />
+        <h3 className="text-lg">La página que busca no existe</h3>
+        <Link className="text-primary-100" to="/">
+          Volver
+        </Link>
+      </div>
+    </MainLayout>
   );
 }
