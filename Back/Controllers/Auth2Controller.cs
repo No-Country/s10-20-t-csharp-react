ec6b7e98d43e8ce507c4160bc1b3fe60ae8430ca
Daniel Torres <danto@hotmail.com>
2023-09-06 21:31:34 -0500

    Back/auth redirect (#16)
    * Me endoint
    
    * Fix redirects + Auth IsAuthenticated
    
    * auth with redirect

--- Back/Controllers/Auth2Controller.cs
+++ Back/Controllers/Auth2Controller.cs
@@ -7,6 +7,9 @@ using System.Security.Claims;
 using s10.Back.Data.Repositories;
 using quejapp.Models;
 using s10.Back.Data;
+using Microsoft.AspNetCore.Http.Extensions;
+using CloudinaryDotNet;
+using s10.Migrations;
 
 namespace s10.Back.Controllers
 {
@@ -23,30 +26,31 @@ namespace s10.Back.Controllers
 
         public ActionResult Home()
         {
-            return Ok( new { loggedOut = true });
+            return Ok("home");
         }
 
         /// <summary>
         /// Login with a provides
         /// </summary>
         /// <returns></returns>
-        public async Task<ActionResult> Login()
+        public async Task<ActionResult> Login(string mode = "prod", string? Redirect_Uri = null)
         {
             //TODO Customize for other providers
             var result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             if (result.Succeeded)
             {
-                return RedirectToAction(nameof(GoogleResponse));
+                return RedirectToAction(nameof(GoogleResponse), new { mode , Redirect_Uri });
             }
+
             await HttpContext.ChallengeAsync(GoogleDefaults.AuthenticationScheme, new AuthenticationProperties()
             {
-                RedirectUri = Url.Action("GoogleResponse"),
+                RedirectUri = Url.Action("GoogleResponse", new { mode, Redirect_Uri }),
             });
             return new EmptyResult();
         }
 
         // [Route("/signin-google")]
-        public async Task<IActionResult> GoogleResponse()
+        public async Task<IActionResult> GoogleResponse(string mode, string? Redirect_Uri = null)
         {
             var urlBase = HttpContext.Request.QueryString;
 
@@ -60,7 +64,7 @@ namespace s10.Back.Controllers
                     claim.Value
                 });
             var posibleUser = claims.Where(c => c.Type == System.Security.Claims.ClaimTypes.Email);
-            if(posibleUser.Any())
+            if (posibleUser.Any())
             {
                 var unitOfWork = new UnitOfWork(_context);
                 int changes = 0;
@@ -70,34 +74,46 @@ namespace s10.Back.Controllers
                     {
                         Email = posibleUser.First().Value,
                         Name = claims.Where(c => c.Type == ClaimTypes.Name).First().Value,
-                        GivenName = (claims.FirstOrDefault( x => x.Type == ClaimTypes.GivenName)?.Value)??""
+                        GivenName = (claims.FirstOrDefault(x => x.Type == ClaimTypes.GivenName)?.Value) ?? ""
                     };
                     unitOfWork.AppUsers.Add(newUser);
                     changes = await unitOfWork.Complete();
                 }
             }
 
-
-            //redirect
-            var r = HttpContext.Request;
-            var url = r.Scheme +"://" + r.Host;
-#if DEBUG
-            //only webapi redirect to swagger
-            if(r.Host.Port.Equals(5001))
+            //redirect_uri cannot be from  outside domain
+            //sanitize from sqli
+            //xss
+            //etc
+            //https://learn.microsoft.com/en-us/aspnet/core/security/cross-site-scripting?view=aspnetcore-7.0
+            if (Redirect_Uri != null)
             {
-                return Redirect($"{url}/swagger");
-            }
-            return Redirect($"{url}/");
-#else
-            //Relese with spa redirect to Wall?
-            return Redirect($"{url}/");
-#endif
-        }
+                Redirect_Uri = Redirect_Uri.Contains("http") ? null : Redirect_Uri;
 
-        [Authorize]
-        public IActionResult Data()
-        {
-            return Ok(HttpContext.User.Identities.First().Claims.Select(x => x.Value));
+            }
+
+            var redirectTo = Redirect_Uri ?? "report";
+            if (mode == "dev")
+            {
+                var client = "https://localhost:5173";
+                return Redirect($"{client}/{redirectTo}");
+            }
+            return Redirect($"/{redirectTo}");
+
+            //            //redirect
+            //            var r = HttpContext.Request;
+            //            var url = r.Scheme +"://" + r.Host;
+            //#if DEBUG
+            //            //only webapi redirect to swagger
+            //            if(r.Host.Port.Equals(5001))
+            //            {
+            //                return Redirect($"{url}/swagger");
+            //            }
+            //            return Redirect($"{url}/");
+            //#else
+            //            //Relese with spa redirect to Wall?
+            //            return Redirect($"{url}/");
+            //#endif
         }
 
         [Authorize]
