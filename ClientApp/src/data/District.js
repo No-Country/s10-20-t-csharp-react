d0988655dc363f9be2c545ecf6a2c4a21f618f9d
Ana Juárez <lisa13suarez@gmail.com>
2023-09-07 18:25:33 -0600

    Data de los distritos

---
+++ ClientApp/src/data/District.js
@@ -0,0 +1,99 @@
+const District = [
+    {
+        "District_ID": 1,
+        "Name": "Tolosa",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 2,
+        "Name": "Ringuelet",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 3,
+        "Name": "Manuel B. Gonnet",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 4,
+        "Name": "City Bell",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 6,
+        "Name": "Villa Elisa",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 7,
+        "Name": "Villa Elvira",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 8,
+        "Name": "El Peligro",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 9,
+        "Name": "Altos de San Lorenzo",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 10,
+        "Name": "Joaquín Gorina",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 11,
+        "Name": "Arturo Seguí",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 12,
+        "Name": "José Hernández",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 13,
+        "Name": "San Carlos",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 15,
+        "Name": "Melchor Romero",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 16,
+        "Name": "Abasto",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 17,
+        "Name": "Los Hornos",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 18,
+        "Name": "Lisandro Olmos",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 19,
+        "Name": "Ángel Etcheverry",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 20,
+        "Name": "Villa Garibaldi",
+        "Locality_Name": "La Plata"
+    },
+    {
+        "District_ID": 22,
+        "Name": "Ignacio Correas",
+        "Locality_Name": "La Plata"
+    }
+]
+
+export default District
