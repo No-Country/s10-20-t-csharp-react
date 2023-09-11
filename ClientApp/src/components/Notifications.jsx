82f78d48be3b73de043f10478b770fefd73d404a
Ana Juárez <lisa13suarez@gmail.com>
2023-09-10 21:08:18 -0600

    Actualizando tamaño

--- ClientApp/src/components/Notifications.jsx
+++ ClientApp/src/components/Notifications.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react"
 
 const Notifications = () => {
-    const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <div className={`absolute top-16 -ml-40 shadow-2xl bg-white h-fit rounded-lg`}>
+        <div className={`mt-10 absolute -ml-60 shadow-2xl bg-white h-fit rounded-lg w-[22%]`}>
             <ul className="relative p-4 z-10">
-                <p className="text-black font-medium w-[200px] text-center">Notificaciones</p>
+                <p className="text-black font-medium text-center w-full">Notificaciones</p>
                 <li className="text-sm mt-5 text-black"> <span className="font-semibold mr-2 text-black">Lorena Casas</span>Ha comentado una queja</li>
                 <hr className="mt-4 border-[1px] border-black"/>
                 <li className="text-sm mt-5 text-black"> <span className="font-semibold mr-2 text-black">Juan Rodriguez</span>Ha comentado una queja</li>
