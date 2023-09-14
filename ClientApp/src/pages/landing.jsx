78487deec2dbf1bb9e148c24b09248c22dfb5961
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-09-14 12:03:25 -0300

    Style: change the styles of the features section

--- ClientApp/src/pages/landing.jsx
+++ ClientApp/src/pages/landing.jsx
@@ -1,16 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { MainLayout } from "../layout/MainLayout";
+import HelpIcon from "../assets/icons/help-information-question.svg";
+import SpeakerIcon from "../assets/icons/broadcast.svg";
+import PressIcon from "../assets/icons/press.svg";
+import LikeIcon from "../assets/icons/like.svg";
 import puzzle from "../assets/puzzle.png";
 import flechaAbajo from "../assets/icons/flecha.svg";
 import logo from "../assets/logo.svg";
-import { MainLayout } from "../layout/MainLayout";
 
 const featuresItems = [
-  "Ayudá a visibilizar las problematicas frecuentes en los bienes publicos de tu ciudad",
-  "Enterate de los reportes de otros ciudadanos",
-  "Controbuí a mantener los espacios comunes en optimas condiciones",
-  "Sé responsable...",
+  <div id="first-row" className="flex gap-8 w-42">
+    <div className="flex gap-2 items-center">
+      <img width="80" height="80" src={HelpIcon} alt="Ícono sobre ayudar" />
+      <p className="w-[28ch]">
+        Ayudá a visibilizar las problematicas frecuentes en los bienes públicos
+        de tu ciudad,
+      </p>
+    </div>
+
+    <div className="flex gap-2 items-center">
+      <img width="80" height="80" src={SpeakerIcon} alt="Ícono de un altavoz" />
+      <p className="w-[28ch]">Entérate de los reportes de otros ciudadanos,</p>
+    </div>
+  </div>,
+  <div id="second-row" className="flex gap-8">
+    <div className="flex gap-2 items-center">
+      <img
+        width="80"
+        height="80"
+        src={PressIcon}
+        alt="Ícono sobre una mano presionando"
+      />
+      <p className="w-[28ch]">
+        Contribuí a mantener los espacios comunes en óptimas condiciones
+      </p>
+    </div>
+    <div className="flex gap-2 items-center">
+      <img
+        width="80"
+        height="80"
+        src={LikeIcon}
+        alt="Ícono de pulgares arriba"
+      />
+      <p className="w-[28ch]">Sé responsable...</p>
+    </div>
+  </div>,
 ];
 
 export default function Landing() {
@@ -19,7 +55,7 @@ export default function Landing() {
       <main className="px-28">
         <div
           id="hero-section"
-          className="h-screen flex flex-col items-center gap-4 sm:gap-2 justify-center"
+          className="h-screen flex flex-col items-center gap-4 justify-center"
         >
           <div className="flex justify-center items-center gap-4">
             <div id="hero-section__img">
@@ -28,19 +64,15 @@ export default function Landing() {
                 height="500"
                 src={puzzle}
                 alt="Personas sobre un rompecabezas"
-                className="sm:w-4"
+                className=""
               />
             </div>
             <div
               id="hero-section__content"
               className="flex flex-col justify-center items-center gap-6"
             >
-              <img
-                className="w-96 sm:w-56"
-                src={logo}
-                alt="Logo de la página"
-              />
-              <p className="w-[30ch] text-center text-xl sm:text-sm text-black">
+              <img className="w-96" src={logo} alt="Logo de la página" />
+              <p className="w-[30ch] text-center text-xl text-black">
                 Tu aporte como ciudadano ayuda a visibilizar problemas en los
                 bienes de uso públicos
               </p>
@@ -62,7 +94,7 @@ export default function Landing() {
           </div>
           <div>
             <img
-              className="mt-20 sm:w-12"
+              className="mt-20"
               src={flechaAbajo}
               alt="Flecha apuntando hacia abajo"
               width="50"
@@ -71,14 +103,11 @@ export default function Landing() {
           </div>
         </div>
 
-        <section className="flex flex-col items-center gap-8 py-10">
-          <ul className="flex flex-col gap-4">
+        <section className="flex justify-center items-center gap-8 py-16">
+          <ul className="flex flex-col gap-10 items-center justify-center">
             {featuresItems.map((item, id) => (
-              <li
-                key={id}
-                className="text-title-lg sm:text-base sm:w-[25ch] sm:text-center text-black"
-              >
-                {item}
+              <li key={id} className="text-lg text-black w-[70ch]">
+                {<p>{item}</p>}
               </li>
             ))}
           </ul>
