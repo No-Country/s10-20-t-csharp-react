29275fd9246fe939aa86f36728878225da7bf13d
Ana Juárez <lisa13suarez@gmail.com>
2023-09-14 00:51:21 -0600

    Error al crear queja

--- ClientApp/src/components/Modal.jsx
+++ ClientApp/src/components/Modal.jsx
@@ -1,26 +1,49 @@
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import District from '../data/District'
 import ImageUpload from './ImageUpload'
+import Category from '../data/Category'
 
 const Modal = () => {
     const { register, handleSubmit, formState: { errors } } = useForm()
     const [isOpen, setIsOpen] = useState(false)
-    const [ubication, setUbication] = useState(false)
+    const [category, setCategory] = useState(1)
+    const [ubication, setUbication] = useState(1)
     const [close, setClose] = useState(false)
+    let token = ''
+
+    
+    useEffect(() => {
+        axios.post('https://s10nc.somee.com/api/Auth/login', {
+            user: "user1@example.com",
+            password: "string1"
+        }).then(res => {
+            console.log("respuesta login: ", res)
+            token = res.data.token
+        })
+    }, [])
 
     const onSubmit = data => {
-        console.log("Click")
         //data.district_ID = `${ubication}`
-        data.location.latitude = 0
-        data.location.longitude = 0
-        data.photoAdress = localStorage.getItem('complaintImage')
+     //data.complaint_ID = category
+       /* data.complaint_ID =category
+        data.latitude = 0
+        data.longitude = 0*/
+        //data.photoAdress = `${localStorage.getItem('complaintImage')}`
         console.log(data)
-        axios.post('https://s10nc.somee.com/api/Quejas', data).then(res => {
-            console.log("Respuesta API: ", res)
-        })
-        console.log("id: ", ubication)
+        console.log("token ",token)
+
+        const instance = axios.create({
+            baseURL: 'https://s10nc.somee.com/api',
+            headers: { Authorization: `Bearer ${token}` }
+        });
+
+        instance.get('/Auth/IsAuthenticated').then(res => console.log('autenticate'))
+
+
+        instance.post('/Quejas', data).then(res => console.log("peticion: ",res))
+        setIsOpen(!isOpen)
     }
 
     return (
@@ -31,27 +54,12 @@ const Modal = () => {
                         <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
                     </svg>
                 </div>
-                {/*<div className='flex justify-start items-center w-full'>
-                    <details className="dropdown">
-                        <summary className="m-1 btn capitalize bg-white text-black hover:bg-[#1E3756] hover:text-white" >Seleccione ubicación</summary>
-                        <ul className={`p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52`}>
-                            {
-                                District.map((item) => (
-                                    <li key={item.District_ID} onClick={() => setClose(true)}><a className="text-xs" onClick={() => {
-                                        setUbication(item.District_ID)
-                                       // setClose(!close)
-                                    }}>{item.Name}</a></li>
-                                ))
-                            }
-                        </ul>
-                    </details>
-                        </div>*/}
                 <div className="w-full">
                     <label htmlFor="#" className="text-black">Título:</label>
                     <input type="text" name="#" id="#" className="p-2 w-full rounded-xl border-black border-[1.8px] bg-white" {...register("title", { required: true })} />
                 </div>
                 <div className="w-full relative">
-                    <input type="text" name="#" id="#" className="p-2 w-full rounded-xl bg-white border-black border-[1.8px] placeholder:text-black" placeholder="Categoría" {...register("category_ID", { required: true })} />
+                    <input type='text' className="p-2 w-full rounded-xl border-black border-[1.8px] bg-white" {...register("complaint_ID", { required: true })}/>
                 </div>
                 <div className="w-full relative">
                     <textarea rows={10} cols={55} className="rounded-xl bg-white border-black border-[1.5px] p-2 placeholder:text-black" placeholder="Descripción" {...register("text", { required: true })} />
