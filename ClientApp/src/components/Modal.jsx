e632cfdfadd99b649553fa5385109591e9de09d5
Ana Juárez <lisa13suarez@gmail.com>
2023-09-13 20:28:03 -0600

    update

--- ClientApp/src/components/Modal.jsx
+++ ClientApp/src/components/Modal.jsx
@@ -11,12 +11,15 @@ const Modal = () => {
     const [close, setClose] = useState(false)
 
     const onSubmit = data => {
-        data.district_ID = `${ubication}`
+        console.log("Click")
+        //data.district_ID = `${ubication}`
+        data.location.latitude = 0
+        data.location.longitude = 0
         data.photoAdress = localStorage.getItem('complaintImage')
         console.log(data)
-        /*axios.post('https://s10nc.somee.com/api/Quejas', data).then(res => {
+        axios.post('https://s10nc.somee.com/api/Quejas', data).then(res => {
             console.log("Respuesta API: ", res)
-        })*/
+        })
         console.log("id: ", ubication)
     }
 
@@ -28,10 +31,10 @@ const Modal = () => {
                         <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
                     </svg>
                 </div>
-                <div className='flex justify-start items-center w-full'>
+                {/*<div className='flex justify-start items-center w-full'>
                     <details className="dropdown">
                         <summary className="m-1 btn capitalize bg-white text-black hover:bg-[#1E3756] hover:text-white" >Seleccione ubicación</summary>
-                        <ul className={`p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52 ${close ? 'flex' : 'hidden'}`}>
+                        <ul className={`p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52`}>
                             {
                                 District.map((item) => (
                                     <li key={item.District_ID} onClick={() => setClose(true)}><a className="text-xs" onClick={() => {
@@ -42,7 +45,7 @@ const Modal = () => {
                             }
                         </ul>
                     </details>
-                </div>
+                        </div>*/}
                 <div className="w-full">
                     <label htmlFor="#" className="text-black">Título:</label>
                     <input type="text" name="#" id="#" className="p-2 w-full rounded-xl border-black border-[1.8px] bg-white" {...register("title", { required: true })} />
