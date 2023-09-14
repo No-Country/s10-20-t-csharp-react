7f81250dbb10e6a19fa631c0b8176daa733136a3
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-09-13 21:15:06 -0300

    Add options to the request

--- ClientApp/src/store/userContext.jsx
+++ ClientApp/src/store/userContext.jsx
@@ -1,90 +1,107 @@
-import React, { createContext, useState, useEffect } from 'react';
+import { createContext, useState, useEffect } from "react";
 
-const UserContext = createContext({ 
-    userId: null,             
-    updateUser: () => {},
-    userProfileImage: null,
-    updateUserProfileImage: () => {},
-    userName: null,
-    updateUserName: () => {},
-    userEmail: null,
-    updateUserEmail: () => {}         
+const UserContext = createContext({
+  userId: null,
+  updateUser: () => {},
+  userProfileImage: null,
+  updateUserProfileImage: () => {},
+  userName: null,
+  updateUserName: () => {},
+  userEmail: null,
+  updateUserEmail: () => {},
+  userDescription: null,
+  updateUserDescription: () => {},
 });
 
+const UserProvider = ({ children }) => {
+  const [userId, setUserId] = useState(() => {
+    const storedUserId = sessionStorage.getItem("userId");
+    return storedUserId !== null ? storedUserId : null;
+  });
 
-const UserProvider = ({ children }) => {    
-                    
+  const [userProfileImage, setUserProfileImage] = useState(() => {
+    const storedUserProfileImage = sessionStorage.getItem("userProfileImage");
+    return storedUserProfileImage !== null ? storedUserProfileImage : null;
+  });
 
-const [userId, setUserId] = useState(() => {          
-     const storedUserId = sessionStorage.getItem('userId');
-     return storedUserId !== null ? storedUserId : null;    
- });
+  const [userName, setUserName] = useState(() => {
+    const storedUserName = sessionStorage.getItem("userName");
+    return storedUserName !== null ? storedUserName : null;
+  });
 
- const [userProfileImage, setUserProfileImage] = useState(() => { 
-      const storedUserProfileImage = sessionStorage.getItem('userProfileImage');
-     return storedUserProfileImage !== null ? storedUserProfileImage : null;    
+  const [userEmail, setUserEmail] = useState(() => {
+    const storedUserEmail = sessionStorage.getItem("userEmail");
+    return storedUserEmail !== null ? storedUserEmail : null;
+  });
 
- })
+  const [userDescription, setUserDescription] = useState(() => {
+    const storedUserDescription = sessionStorage.getItem("userDescription");
+    return storedUserDescription !== null ? storedUserDescription : null;
+  });
 
- const [userName, setUserName] = useState(() => { 
-  const storedUserName= sessionStorage.getItem("userName")
-  return storedUserName!== null ? storedUserName: null;
- })
+  const updateUser = id => {
+    setUserId(id);
+    sessionStorage.setItem("userId", id);
+  };
 
+  const updateUserProfileImage = x => {
+    setUserProfileImage(x);
+    sessionStorage.setItem("userProfileImage", x);
+  };
 
+  const updateUserName = name => {
+    setUserName(name);
+    sessionStorage.setItem("userName", name);
+  };
 
- const [userEmail, setUserEmail] = useState(() => { 
-  const storedUserEmail = sessionStorage.getItem("userEmail")
-  return storedUserEmail !== null ? storedUserEmail : null
- })
+  const updateUserEmail = x => {
+    setUserEmail(x);
+    sessionStorage.setItem("userEmail", x);
+  };
 
+  const updateUserDescription = x => {
+    setUserDescription(x);
+    sessionStorage.setItem("userDescription", x);
+  };
 
-const updateUser = (id) => {                   
-    setUserId(id)
-    sessionStorage.setItem('userId', id);     
-};
-
-const updateUserProfileImage = (x) => { 
-    setUserProfileImage(x)
-    sessionStorage.setItem("userProfileImage", x)
-}
-
-const updateUserName = (name) => { 
-  setUserName(name)
-  sessionStorage.setItem("userName", name)
-}
-
-
-const updateUserEmail = (x) => { 
-  setUserEmail(x)
-  sessionStorage.setItem("userEmail", x)
-}
-
-useEffect(() => {
-    const handleStorageChange = (event) => {    
-      if (event.key === 'userId') {            
+  useEffect(() => {
+    const handleStorageChange = event => {
+      if (event.key === "userId") {
         setUserId(event.newValue);
-      } else if (event.key === 'userProfileImage') {            
+      } else if (event.key === "userProfileImage") {
         setUserProfileImage(event.newValue);
-      } else if (event.key === 'userName') {            
+      } else if (event.key === "userName") {
         setUserName(event.newValue);
-      } else if (event.key === "userEmail") { 
-        setUserEmail(event.newValue)
+      } else if (event.key === "userEmail") {
+        setUserEmail(event.newValue);
+      } else if (event.key === "userDescription") {
+        setUserDescription(event.newValue);
       }
     };
-    window.addEventListener('storage', handleStorageChange); 
+    window.addEventListener("storage", handleStorageChange);
     return () => {
-      window.removeEventListener('storage', handleStorageChange); 
+      window.removeEventListener("storage", handleStorageChange);
     };
   }, []);
 
   return (
-    <UserContext.Provider value={{ userId, updateUser, userProfileImage, updateUserProfileImage, userName, updateUserName, userEmail, updateUserEmail }}>
+    <UserContext.Provider
+      value={{
+        userId,
+        updateUser,
+        userProfileImage,
+        updateUserProfileImage,
+        userName,
+        updateUserName,
+        userEmail,
+        updateUserEmail,
+        userDescription,
+        updateUserDescription,
+      }}
+    >
       {children}
     </UserContext.Provider>
   );
 };
 
 export { UserContext, UserProvider };
-
-          
